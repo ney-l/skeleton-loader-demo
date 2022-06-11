35af24296e09976c2606930580f1b1cbdfb87ec6
Ney Lanjewar <neylanjewar@gmail.com>
2022-06-11 13:30:30 +0530

    feat(api routes): :necktie: adds api routes

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { user, videos } from './fakeData';
 
 dotenv.config();
 
@@ -8,6 +9,18 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Skeleton Loader Demo API!');
+});
+
+app.get('/api/users/1', (req: Request, res: Response) => {
+  setTimeout(() => {
+    res.json(user);
+  }, 3000);
+});
+
+app.get('/api/videos/1', (req: Request, res: Response) => {
+  setTimeout(() => {
+    res.json(videos);
+  }, 5000);
 });
 
 app.listen(port, () => {
